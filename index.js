817a2fdcbe97cb2c3e67bcf82faec8b4422009b1
BOTAPRI <78464840+BOTAPRI@users.noreply.github.com>
2021-02-14 14:51:14 +0000

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const {
    waChatKey,
    mentionedJid,
    processTime,
-} = require('@adiwajshing/baileys');
+} = require('@apriantokeristian/baileys');
 const qrcode = require("qrcode-terminal") 
 const moment = require("moment-timezone") 
 const fs = require("fs") 
@@ -35,8 +35,8 @@ const { ind } = require('./language')
 /********** MENU SETTING **********/
 const vcard = 'BEGIN:VCARD\n' 
             + 'VERSION:3.0\n' 
-            + 'FN:Affis Admin\n' 
-            + 'ORG: Pengembang XBot;\n' 
+            + 'FN:Apri Admin\n' 
+            + 'ORG: Pengembang zxbot|APRI;\n' 
             + 'TEL;type=CELL;type=VOICE;waid=6282334297175:+62 823-3429-7175\n' 
             + 'END:VCARD' 
 blocked = []   
@@ -47,7 +47,7 @@ cr = '*BOT INI SUDAH TERVERIFIKASI*'
 /*************************************/
 
 /******** OWNER NUMBER**********/
-const ownerNumber = ["62895710073737@s.whatsapp.net","6282334297175@s.whatsapp.net"] 
+const ownerNumber = ["6282191563771@s.whatsapp.net","6282191563771@s.whatsapp.net"] 
 /************************************/
 
        
